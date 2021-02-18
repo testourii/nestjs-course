37e58915fd83c2b55e2b411c281f4ad410eae23c
Houcem Testouri <htestouri@mail.arsd.dc>
2021-02-18 13:46:45 +0100

    videos2-12

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { title } from 'process';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('descreption') description: string,
+  ) {
+    this.tasksService.createTask(title, description);
+  }
+}
